da8a4296e1ecee072b062f01d0a45ee38226cfd4
Jean-PaulD <jpdebuys@gmail.com>
2017-10-15 23:11:34 +0200

    front end still in progress

---
+++ src/scripts/jsController.js
@@ -0,0 +1,19 @@
+/**
+function createUser(){
+    alert("it worked");
+}
+*/
+/**
+$(document).ready(function() {
+
+
+
+    $.ajax({
+        url: "http://rest-service.guides.spring.io/greeting"
+    }).then(function(data) { alert("i am here");
+        $('.greeting-id').append(data.id);
+        $('.greeting-content').append(data.content);
+    });
+
+});
+*/
